aef943bfdd4ed69249e3a0b11c6090a89a53510d
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2024-01-13 17:28:09 +0300

    feat: add system_property native

--- cmd/samples/cgo/main.go
+++ cmd/samples/cgo/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"dxfeed-graal-go-api/internal/isolate"
-	"dxfeed-graal-go-api/pkg/api"
 	"math"
 	"time"
 )
 
 func main() {
-	isolate.NewEndpoint(api.Feed).Connect("demo.dxfeed.com:7300")
+	//native.NewEndpoint().Connect("demo.dxfeed.com:7300")
 	time.Sleep(time.Duration(math.MaxInt64))
 }
